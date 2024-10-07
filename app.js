26161071a3458ee62107f57dee715457eaec01ba
DeanNguyen98 <nctuan.hn@gmail.com>
2024-10-07 22:42:44 +1100

    test messages for index.ejs

--- app.js
+++ app.js
@@ -4,8 +4,23 @@ const PORT = 3000;
 
 app.set('view engine', 'ejs');
 
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+      },
+      {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+      }
+]
 app.get('/', (req, res) => {
-    res.render('pages/index')
+    res.render('pages/index', {
+        title: 'Mini Message Board',
+        messages: messages
+    })
 })
 
 app.listen(PORT, () => {
