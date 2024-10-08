ca507b1127146cfd9ae30c4572b25c9a60cbb39f
DeanNguyen98 <nctuan.hn@gmail.com>
2024-10-08 19:05:33 +1100

    update app.js for newForm input

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+//Middleware to parse URL-encoded data
+app.use(express.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
 
 const messages = [
@@ -22,6 +25,20 @@ app.get('/', (req, res) => {
         messages: messages
     })
 })
+
+app.get('/form', (req, res) => {
+    res.render('pages/newForm', {
+        title:'New Message'
+    })
+})
+
+app.post('/form', (req, res) => {
+    const {userName, messageText} = req.body;
+    messages.push({text: messageText, user:userName, added: new Date()});
+    res.redirect('/');
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`App listening at port ${PORT}`);
